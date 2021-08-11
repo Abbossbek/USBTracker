2348cd448f2f5ece560c143c14583840f463ff85
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-08-12 00:53:50 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace USBTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbUSBs.ItemsSource = DriveInfo.GetDrives();
+        }
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Adds the windows message processing hook and registers USB device add/removal notification.
+            HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            if (source != null)
+            {
+                var windowHandle = source.Handle;
+                source.AddHook(HwndHandler);
+                UsbNotification.RegisterUsbDeviceNotification(windowHandle);
+            }
+        }
+
+        /// <summary>
+        /// Method that receives window messages.
+        /// </summary>
+        private IntPtr HwndHandler(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            if (msg == UsbNotification.WmDevicechange)
+            {
+                switch ((int)wparam)
+                {
+                    case UsbNotification.DbtDeviceremovecomplete:
+                        Usb_DeviceRemoved(); // this is where you do your magic
+                        break;
+                    case UsbNotification.DbtDevicearrival:
+                        Usb_DeviceAdded(); // this is where you do your magic
+                        break;
+                }
+            }
+
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+        private void Usb_DeviceAdded()
+        {
+            lbUSBs.ItemsSource = DriveInfo.GetDrives();
+        }
+
+        private void Usb_DeviceRemoved()
+        {
+            lbUSBs.ItemsSource = DriveInfo.GetDrives();
+        }
+    }
+}
